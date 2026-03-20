2c038bb43d2d787b6a9bd29a41d88544671b542a
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-03-20 15:45:58 +0530

    updated code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/user/user.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy.users';
+
+const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {
+  selected_users=DUMMY_USERS[randomIndex];
+
+  //getter
+
+  get imagePath(){
+    return 'assets/user/' + this.selected_users.avatar
+  }
+}
