b2bcf3e742a925a01f45f9c93f3e59210b1cf424
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-03-20 17:55:59 +0530

    implementing signals

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy.users';
 
 const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
@@ -10,18 +10,20 @@ const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
   styleUrl: './user.css',
 })
 export class User {
-  selected_users=DUMMY_USERS[randomIndex];
+  selected_users=signal(DUMMY_USERS[randomIndex]);
+  imagePath=computed(()=>'assets/user/'+this.selected_users().avatar)
 
   //getter
 
-  get imagePath(){
-    return 'assets/user/' + this.selected_users.avatar
-  }
+  // get imagePath(){
+  //   return 'assets/user/' + this.selected_users.avatar
+  // }
 
   onSelectUser(){
     //Managing Data
     const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
-    this.selected_users=DUMMY_USERS[randomIndex]
+    // this.selected_users=DUMMY_USERS[randomIndex]
+    this.selected_users.set(DUMMY_USERS[randomIndex])
 
   }
 }
