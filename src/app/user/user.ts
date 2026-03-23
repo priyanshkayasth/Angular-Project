be9283bc1f65b4f980123af7bc3371bb5e179f42
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-03-23 11:18:19 +0530

    define component inputs

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,7 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy.users';
+import { Component, Input } from '@angular/core';
 
-const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -10,20 +8,15 @@ const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
   styleUrl: './user.css',
 })
 export class User {
-  selected_users=signal(DUMMY_USERS[randomIndex]);
-  imagePath=computed(()=>'assets/user/'+this.selected_users().avatar)
 
-  //getter
+  @Input() avatar!:string
+  @Input() name!:string
 
-  // get imagePath(){
-  //   return 'assets/user/' + this.selected_users.avatar
-  // }
-
+  get imagePath(){
+    return 'assets/user/' + this.avatar
+  }
+  
   onSelectUser(){
-    //Managing Data
-    const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length)
-    // this.selected_users=DUMMY_USERS[randomIndex]
-    this.selected_users.set(DUMMY_USERS[randomIndex])
-
+   
   }
 }
