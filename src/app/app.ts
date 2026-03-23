802ddd93f69d060d74a08491b207b0e9607cad6f
Priyansh Kayastha <priyansh.kayastha2004@gmail.com>
2026-03-23 15:31:30 +0530

    implement @for,if-else

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,7 +14,7 @@ import { Task } from './task/task';
 })
 export class App {
   users=DUMMY_USERS
-  selectedUserId="u1"
+  selectedUserId?:string
 
   get selectedUser(){
     return this.users.find((user)=>user.id===this.selectedUserId)
